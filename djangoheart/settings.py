ec74ff5182dde080ee4dafc45b188ebb840683da
Alamin Musa Magaga <55305080+alaminmagaga@users.noreply.github.com>
2022-06-04 07:48:37 -0700

    Update settings.py

--- djangoheart/settings.py
+++ djangoheart/settings.py
@@ -127,12 +127,11 @@ STATICFILES_DIRS=(
     os.path.join(BASE_DIR,'static'),
 )
 
-<<<<<<< HEAD
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-=======
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
->>>>>>> b84a986942c96461190ceded615495a96222903d
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
